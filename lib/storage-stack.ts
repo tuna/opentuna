b75e8ebb205cbede4dd8023ea098578c11365496
Kane Zhu <me@kane.mx>
2020-07-07 15:43:29 +0800

    feat: add efs storage stack.

---
+++ lib/storage-stack.ts
@@ -0,0 +1,41 @@
+import * as cdk from '@aws-cdk/core';
+import ec2 = require("@aws-cdk/aws-ec2");
+import efs = require("@aws-cdk/aws-efs");
+
+export interface StorageStackProps extends cdk.StackProps {
+  readonly vpcId: string;
+}
+
+export class StorageStack extends cdk.Stack {
+
+  readonly fileSystem: efs.IFileSystem;
+
+  constructor(scope: cdk.Construct, id: string, props: StorageStackProps) {
+    super(scope, id, props);
+
+    const stack = cdk.Stack.of(this);
+
+    const vpc = ec2.Vpc.fromLookup(this, `OpenTunaVpc`, {
+      vpcId: props.vpcId,
+    })
+    this.fileSystem = new efs.FileSystem(this, 'OpenTunaEfsFileSystem', {
+      vpc: vpc,
+      encrypted: false,
+      lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
+      performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
+      throughputMode: efs.ThroughputMode.BURSTING,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+    // TODO only grant connection from particular security groups.
+    this.fileSystem.connections.allowDefaultPortFrom(ec2.Peer.ipv4(vpc.vpcCidrBlock),
+      'allow connect from vpc');
+
+    cdk.Tag.add(this, 'usage', 'storage');
+
+    new cdk.CfnOutput(this, 'FileSystemId', {
+      value: `${this.fileSystem.fileSystemId}`,
+      exportName: `${stack.stackName}-FileSystemId`,
+      description: 'EFS FileSystem id'
+    });
+  }
+}
